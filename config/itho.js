5d731d144b9c3f6d829bf88730d784a035966f0a
Niels Janssen <niels@vliegwerk.com>
2019-12-13 14:34:47 +0100

    Itho import functions should now be called with the location (address UUID) we're importing data for Restructured Itho configuration Refactored Itho CSV import Writing all data points at once per CSV file to prevent InfluxDB connection issues

--- config/itho.js
+++ config/itho.js
@@ -2,34 +2,39 @@ module.exports = {
     csv: {
         import: {
             measurementPrefix: "itho_",
-            sourceTag: "csv",
+            source: "csv",
             importPath: "import/itho",
             processedPath: "import/itho/processed",
             errorPath: "import/itho/error",
             delimiter: ";",
-            energy: {
-                measurement: "energy",
-                data_columns: {
-                    0: "datetime",
+            energy_installation: {                
+                measurement: "energy_installation",
+                units: "kWh",
+                fields: { // Date Time;Total generated energy by the installation after completion;Date Time;Total consumed energy by the installation after completion
+                    0: "timestamp",
                     1: "generated",
                     3: "consumed"
                 }
             },
-            temperature: {
-                measurement: "temperature",
-                data_columns: {
-                    0: "datetime",
-                    1: "outdoor",
-                    3: "indoor",
-                    5: "setpoint"
+            temperature_env: { // Date Time;T high;Date Time;T low;Date Time;T outdoor;Date Time;T indoor;Date Time;T set
+                measurement: "temperature_env",
+                units: "C",
+                fields: {
+                    0: "timestamp",
+                    5: "outdoor",
+                    7: "indoor",
+                    9: "setting"
                 }
-            }            
-        },
-        export: {
-            measurement: "itho",
-            sourceTag: "csv",
-            importPath: "export/itho",
-            delimiter: ";"
+            },
+            temperature_boiler: { // Date Time;T high;Date Time;T low;Date Time;T outdoor;Date Time;T indoor;Date Time;T set
+                measurement: "temperature_boiler",
+                units: "C",
+                fields: {
+                    0: "timestamp",                    
+                    1: "high",
+                    3: "low"
+                }
+            }
         }
     }
 }
--- index.js
+++ index.js
@@ -12,5 +12,6 @@ require('@lib/data')
 // Initialize Itho services
 const itho = require('@lib/itho')
 
-itho.csv.import.energy(__dirname + '/data/itho/20190830/dashboard_energy_3991MA2.csv')
-itho.csv.import.temperature(__dirname + '/data/itho/20190830/dashboard_temps_3991MA2.csv')
+itho.csv.import.importCsvEnergyInstallation('3991MA2',__dirname + '/data/itho/20190830/dashboard_energy_3991MA2.csv')
+//itho.csv.import.temperature(__dirname + '/data/itho/20190830/dashboard_temps_3991MA2.csv')
+itho.csv.import.importCsvTemperatureEnv('3991MA2',__dirname + '/data/itho/20190830/wpu_temps_3991MA2.csv')
