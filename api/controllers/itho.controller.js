b416825417b6ddab68544d4ca4f6cc41413d3260
Niels Janssen <niels@vliegwerk.com>
2019-12-23 10:29:29 +0100

    Added Itho bulk import (/itho/import/bulk/energy and /itho/import/bulk/temperature) Updated Postman collection

--- api/controllers/itho.controller.js
+++ api/controllers/itho.controller.js
@@ -13,21 +13,64 @@ exports.importCsv = (req, res, next) => {
 	if (!energy && !temperature) {
 		res.status(500).json({
 			message: 'no files uploaded'
-		})	
-	} 
+		})
+	}
 
 	if (energy) {
 		const buffer = energy[0].buffer
-		itho.csv.import.importCsvEnergyInstallation(uuid,buffer)
+		itho.csv.import.importCsvEnergyInstallation(uuid, buffer)
 	}
 
 	if (temperature) {
 		const buffer = temperature[0].buffer
-		itho.csv.import.importCsvTemperatureEnv(uuid,buffer)
-		itho.csv.import.importCsvTemperatureBoiler(uuid,buffer)
-	}	
+		itho.csv.import.importCsvTemperatureEnv(uuid, buffer)
+		itho.csv.import.importCsvTemperatureBoiler(uuid, buffer)
+	}
 
 	res.status(200).json({
 		message: 'Import started.'
 	})
+}
+
+exports.bulkImportCsvEnergy = (req, res, next) => {
+	const files = req.files
+
+	if (!files || files.length < 1) {
+		res.status(500).json({
+			error: 'No files uploaded.'
+		})
+	}
+
+	for (let file of files) {
+		const uuid = file.fieldname
+		// TODO Check existance of location in MySQL
+		const buffer = file.buffer
+		itho.csv.import.importCsvEnergyInstallation(uuid, buffer)
+	}
+
+	res.status(200).json({
+		message: 'Import started.'
+	})
+}
+
+exports.bulkImportCsvTemperature = (req, res, next) => {
+	const files = req.files
+
+	if (!files || files.length < 1) {
+		res.status(500).json({
+			error: 'No files uploaded.'
+		})
+	}
+
+	for (let file of files) {
+		const uuid = file.fieldname
+		// TODO Check existance of location in MySQL
+		const buffer = file.buffer
+		itho.csv.import.importCsvTemperatureEnv(uuid, buffer)
+		itho.csv.import.importCsvTemperatureBoiler(uuid, buffer)
+	}
+
+	res.status(200).json({
+		message: 'Import started.'
+	})	
 }
--- api/routes/itho.routes.js
+++ api/routes/itho.routes.js
@@ -7,10 +7,17 @@ const multer = require('multer')
 const storage = multer.memoryStorage()
 const upload = multer({ storage: storage })
 
+// Bulk import Itho energy CSVs for address identified by uuid found in field names of the form
+router.post('/import/bulk/energy', jwt, upload.any(),controller.bulkImportCsvEnergy)
+
+// Bulk import Itho energy CSVs for address identified by uuid found in field names of the form
+router.post('/import/bulk/temperature', jwt, upload.any(),controller.bulkImportCsvTemperature)
+
 // Import Itho CSVs for address identified by uuid
 router.post('/import/:uuid', jwt, upload.fields([
 	{ name: 'energy', maxCount: 1 },
 	{ name: 'temperature', maxCount: 1 }
 ]),controller.importCsv)
+
 
 module.exports = router
